d95eb9245ca26e14453418530a95389b78a05dfc
Stephanie Durocher <sdurocher722@gmail.com>
2022-09-03 20:47:49 -0400

    updated css

--- sources/script.js
+++ sources/script.js
@@ -1,34 +1,64 @@
-//function searchTemperature(response) {
-//let temperature = Math.round(response.data.main.temp);
-// let h5 = document.querySelector("h5");
-//h5.innerHTML = `The temperature for your city is ${temperature}`;
-//}
-
-//the only way the apiURL works is if I have it like this. As soon as I change the appid and declare my key or declare lat/lon it gives me a 401 error code.
-//function findLocation(position) {
-//let apiKey = f1a9cf2d1d5190158a907ba1ed5b96af;
-//let latitude = position.coords.latitude;
-//let longitude = position.coords.longitude;
-//let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=f1a9cf2d1d5190158a907ba1ed5b96af`;
-
-// axios.get(apiURL).then(findLocation);
-//}
-
-//navigator.geolocation.getCurrentPosition(findLocation);
-
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#search-input").value;
-  let units = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f1a9cf2d1d5190158a907ba1ed5b96af&units=${units}`;
-
-  axios.get(apiURL).then(displayCity);
+//part 1
+function retrieveInformation(date) {
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let currentDay = currentTime.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `Current time is ${hours}:${minutes} on ${days[currentDay]}`;
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
+let date = document.querySelector("#date");
+let currentTime = new Date();
+
+//date.innerHTML = retrieveInformation(currentTime);
+
+//let searchForm = document.querySelector("#search-form");
+//let dateElement = document.querySelector("#date");
+
+// Get and display current city or location
+function searchCity(event) {
+  event.preventDefault();
+  getCityName();
+}
+
+function getCityName(position) {
+  let searchInput = document.querySelector("#search-input");
+  let searchCity = document.querySelector("h5");
+  searchCity.innerHTML = searchInput.value;
+  searchCity = searchInput.value;
+
+  let endpoint = "api.openweathermap.org";
+  let apiKey = "21f347fd627fde024ba524524a760ab9";
+  let apiUrl = `https://${endpoint}/data/2.5/weather?q=${searchCity}&units=metric&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(displayCity);
+}
 
 function displayCity(response) {
   let h5 = document.querySelector("h5");
   h5.innerHTML = `${city}`;
 }
+
+// display current location
+function displayCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCurrentLocation);
+}
+
+// Event listeners for buttons
+let searchCityButton = document.querySelector("#search-form");
+searchCityButton.addEventListener("submit", searchCity);
